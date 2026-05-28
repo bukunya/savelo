3a3b81f1cbbcdcd1a8099053632c7803f1727d93
Abdullah Afif Habiburrohman <abdullahafifhabiburrohman@mail.ugm.ac.id>
2026-05-28 22:31:04 +0700

    feat: deployment

--- lib/core/network/api_client.dart
+++ lib/core/network/api_client.dart
@@ -12,11 +12,11 @@ class ApiClient {
   static String get baseUrl {
     if (Platform.isAndroid) {
       // return 'http://10.0.2.2:8000/api';
-      return 'http://127.0.0.1:8000/api';
-      // return 'https://savelobe.afif.dev/api';
+      // return 'http://127.0.0.1:8000/api';
+      return 'https://savelobe.afif.dev/api';
     }
-    return 'http://localhost:8000/api';
-    // return 'https://savelobe.afif.dev/api';
+    // return 'http://localhost:8000/api';
+    return 'https://savelobe.afif.dev/api';
   }
 
   ApiClient._internal() {
